6743c42e40021173165fc11b8042fa82ee7c562c
AnaTorres <113297234+AnaTorres61@users.noreply.github.com>
2025-05-30 09:20:20 -0300

    Initial Commit

---
+++ Sinaliza/SinalizaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinalizaApp.swift
+//  Sinaliza
+//
+//  Created by Ana Flávia Torres do Carmo on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SinalizaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
